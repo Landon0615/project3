df084e32ef055a07e2c6904fb588edf985c18b49
Landon Waddell <landon.waddell@gmail.com>
2021-09-27 10:32:57 -0600

    Trying login form

---
+++ client/src/pages/SearchBooks.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+import Auth from '../utils/auth';
+import { useMutation } from '@apollo/client';
+
+  // create method to search for books and set state on form submit
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!searchInput) {
+      return false;
+    }
+
+
+  return (
+      <Jumbotron fluid className='text-light bg-dark'>
+        <Container>
+          <h1>Search for Books!</h1>
+          <Form onSubmit={handleFormSubmit}>
+            <Form.Row>
+              <Col xs={12} md={8}>
+                <Form.Control
+                  name='searchInput'
+                  value={searchInput}
+                  onChange={(e) => setSearchInput(e.target.value)}
+                  type='text'
+                  size='lg'
+                  placeholder='Search for a book'
+                />
+              </Col>
+              <Col xs={12} md={4}>
+                <Button type='submit' variant='success' size='lg'>
+                  Submit Search
+                </Button>
+              </Col>
+            </Form.Row>
+          </Form>
+        </Container>
+      </Jumbotron>
+  );
+};
+
+export default SearchBooks;
